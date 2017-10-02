8d2cfd4d4b48599bbb388ca3a9c300f9f9191658
DIVYA BHARATHI RAJARAJAN <divyaraja1602@gmail.com>
2017-10-02 17:20:54 +0530

    Update Leap.java

--- Code Kata/Beginner/Set-1/Leap.java
+++ Code Kata/Beginner/Set-1/Leap.java
@@ -8,9 +8,14 @@ public class Leap
 		System.out.println("Enter the year");
 		Scanner s = new Scanner(System.in);
 		int y = s.nextInt();
+		if(y>0)
+		{
 		if(y%4==0)
 			System.out.println(y + " is a Leap year");
 		else
 			System.out.println(y + " is not a leap year");
+		}
+		else
+			System.out.println("Invalid Data");	
 	}
 }
