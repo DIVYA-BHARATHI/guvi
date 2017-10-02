a73cec7f3b71a547723b783ffcaa8f5d05836bf8
DIVYA BHARATHI RAJARAJAN <divyaraja1602@gmail.com>
2017-10-02 17:36:56 +0530

    Update Alphabet.java

--- Code Kata/Beginner/Set-1/Alphabet.java
+++ Code Kata/Beginner/Set-1/Alphabet.java
@@ -11,11 +11,16 @@ public class Alphabet
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the Alphabet");
 		char alpha = s.next().charAt(0);
+		if(Character.isLetter(alpha)){
 		if(alpha == 'a' || alpha == 'e' || alpha == 'i' || alpha == 'o' || alpha == 'u' )
 			System.out.println(alpha + " is a VOWEL");
 		else
 			System.out.println(alpha + " is a CONSONANT");
 		}
+		else
+		{System.out.println("Not an Alphabet");}
+		}
+		
 		catch(Exception e)
 			{	System.out.println(e);
 				}
